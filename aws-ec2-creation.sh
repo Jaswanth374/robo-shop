fa3b7795b92be6c199c5f5c2f9ae2db6eabd60b1
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-02-01 06:41:33 +0530

    push the script to repo

---
+++ aws-ec2-creation.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#Creating aws instances for robo shop project
+
+SG_ID=sg-0fd437546c9954eac
+AMI_ID=ami-0220d79f3f480ecf5
+INST_TYPE=t3.micro
+REGION=us-east-1
+ZONEID=Z07306682ZX5MBM0WBCUJ
+DOMAINNAME=jaswanthdevops.online
+# Run the instance creation command
+
+for instance in $@
+do
+ INSTANCE=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type $INST_TYPE \
+    --security-group-ids $SG_ID \
+    --region $REGION \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+if [ $INSTANCE -eq frontned ]; then
+             IP=$(aws ec2 describe-instances \
+                     --instance-ids $INSTANCE \
+                     --query 'Reservations[].Instances[].PublicIpAddress' \
+                     --output text)
+            RECORD_NM="$INSTANCE.$DOMAINNAME" #Public domain name
+    else
+             IP=$(aws ec2 describe-instances \
+                     --instance-ids $INSTANCE \
+                     --query 'Reservations[].Instances[].PrivateIpAddress' \
+                     --output text)
+             RECORD_NM="$INSTANCE.$DOMAINNAME" #Private domain name
+    fi
+echo "IP Address: $IP"
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONEID \
+    --change-batch '
+{
+  "Comment": "Update A record",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'$INSTANCE'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "'$IP'"
+          }
+        ]
+      }
+    }
+  ]
+}
+'
+echo "Record updated for $INSTANCE"
+
+done
+
+
+
+
