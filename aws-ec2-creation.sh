f4fb50bfb84e620210daa15643c879d4e6505086
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-02-01 06:57:19 +0530

    push the script to repo

--- aws-ec2-creation.sh
+++ aws-ec2-creation.sh
@@ -10,25 +10,25 @@ DOMAINNAME=jaswanthdevops.online
 
 for instance in $@
 do
- INSTANCE=$(aws ec2 run-instances \
+ INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type $INST_TYPE \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text)
-if [ $INSTANCE -eq "frontned" ]; then
+if [ $instance -eq "frontned" ]; then
              IP=$(aws ec2 describe-instances \
-                     --instance-ids $INSTANCE \
+                     --instance-ids $INSTANCE_ID \
                      --query 'Reservations[].Instances[].PublicIpAddress' \
                      --output text)
-            RECORD_NM="$INSTANCE.$DOMAINNAME" #Public domain name
+            RECORD_NM="$instance.$DOMAINNAME" #Public domain name
     else
              IP=$(aws ec2 describe-instances \
-                     --instance-ids $INSTANCE \
+                     --instance-ids $INSTANCE_ID \
                      --query 'Reservations[].Instances[].PrivateIpAddress' \
                      --output text)
-             RECORD_NM="$INSTANCE.$DOMAINNAME" #Private domain name
+             RECORD_NM="$instance.$DOMAINNAME" #Private domain name
     fi
 echo "IP Address: $IP"
 aws route53 change-resource-record-sets \
